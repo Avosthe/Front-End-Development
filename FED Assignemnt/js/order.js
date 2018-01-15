a53ea46d5845a2ff31606109344e053fef37ac1c
Yao Wei Zachary Chng <s10179031@connect.np.edu.sg>
2018-01-15 22:51:44 +0800

    made changes to the submit and reset

--- FED Assignemnt/js/order.js
+++ FED Assignemnt/js/order.js
@@ -3,13 +3,19 @@ function submit() {
     var elem = document.getElementById("animate");
     var pos = 0;
     var id = setInterval(frame, 10);
+    document.getElementById("animate").innerHTML = "Form is proccessing";
     function frame() {
         if (pos == 350) {
             clearInterval(id);
             alert("Your ordered has been delivered")
+            location.reload(true);
         } else {
             pos++;
             elem.style.left = pos + 'px';
         }
     }
+}
+
+function reload() {
+    location.reload(true);
 }
