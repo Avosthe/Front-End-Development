a91fa844e355ce3a32f255b4d20c2c7aa925b782
Yao Wei Zachary Chng <s10179031@connect.np.edu.sg>
2018-01-17 15:12:43 +0800

    made submit and onclick added some functions made some changes to css

--- FED Assignemnt/js/order.js
+++ FED Assignemnt/js/order.js
@@ -1,21 +1,46 @@
 // JavaScript source code
-function submit() {
+function date() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1;
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    document.getElementById("date").setAttribute("min", today);
+}
+
+function time(){
+    var today = new Date();
+    var day = today.getDay();
+    if (day == 0) {
+        document.getElementById("time").setAttribute("min","11:00")
+        document.getElementById("time").setAttribute("max","15:00");
+    }
+    if (day == 6) {
+        document.getElementById("time").setAttribute("min","11:00");
+        document.getElementById("time").setAttribute("max","16:00");
+    }
+}
+
+function lul() {
     var elem = document.getElementById("animate");
     var pos = 0;
-    var id = setInterval(frame, 10);
-    document.getElementById("animate").innerHTML = "Form is proccessing";
+    var id = setInterval(frame, 2);
     function frame() {
-        if (pos == 350) {
+        if (elem.style.left == window.innerWidth + 'px') {
+            elem.style.visibility = "hidden";
             clearInterval(id);
             alert("Your ordered has been delivered")
             location.reload(true);
         } else {
-            pos++;
+            pos+=3;
             elem.style.left = pos + 'px';
         }
     }
 }
 
-function reload() {
-    location.reload(true);
-}
