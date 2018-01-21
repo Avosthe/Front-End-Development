e1770cd5a0ac4e1facd8c84357e1a60167de089b
Darren Chan <s10179845@connect.np.edu.sg>
2018-01-21 19:33:18 +0800

    edited sidebar

--- FED Assignemnt/headerScript.js
+++ FED Assignemnt/headerScript.js
@@ -32,5 +32,19 @@ function closesidenav() {
     document.getElementById("sideNav").style.width = "0";
 }
 
-
+var check = true;
+function handleScroll() {
+    var nav = document.querySelector("nav");
+    if (check && (window.scrollY >= window.innerHeight)) {
+        nav.style.position = "fixed";
+        nav.classList.toggle("transform");
+        check = false;
+    }
+    if (!(check) && (window.scrollY <= window.innerHeight)) {
+        nav.style.position = "unset";
+        nav.classList.toggle("transform");
+        check = true;
+    }
+}
+window.addEventListener("scroll", handleScroll);
 
